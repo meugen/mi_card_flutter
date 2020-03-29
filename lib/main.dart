24bf2ab3ac3722dff107afa39f2104c37a978272
Yevgeniy Malyshev <meugen.82@gmail.com>
2020-03-29 19:47:10 +0300

    Implemented MiCard app on flutter

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,90 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/DSCN1015_squared.png'),
+              ),
+              Text(
+                'Yevgeniy Malyshev',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              Text(
+                'ANDROID DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20,
+                  fontWeight: FontWeight.w300,
+                  letterSpacing: 5,
+                  color: Colors.white
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+//                  contentPadding: EdgeInsets.all(0),
+                  leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal
+                  ),
+                  title: Text(
+                    '+38 (123) 456 78 90',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20
+                    ),
+                  ),
+                )
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'meugen@example.com',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20
+                    ),
+                  )
+                )
+              )
+            ],
+          )
+        )
       ),
-    ),
-  );
+    );
+  }
 }
